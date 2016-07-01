9d2a272eaa01f0cdbcf682910dd4bb31d52d3a9c
justine <justine.winnie@gmail.com>
2016-07-01 14:05:27 -0700

    styling

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,46 +17,63 @@ $(document).ready(function () {
 
   var game = new TicTacToe()
 
-  // TicTacToe.prototype.first_player = function (player_img) {
-  //   // player autoset as 1/cat
-  //   //
-  //   if (game.current_player === 1) {
-  //     turn.addClass('cat')
-  //     turn.attr("src", "cat.png")
-  //   } else {
-  //     turn.addClass('dog')
-  //     turn.attr("src", "dog.jpg")
-  //     game.current_player = 2
-  //   }
-  // }
-  //
+  TicTacToe.prototype.first_player = function (player_img) {
+    var player_img_choice = $(player_img).find('.image')
+    console.log(player_img_choice)
+    // display both cat and dog
+    // click either cat or dog
+    // choice assigns player no.
+
+    // if (game.current_player === 1) {
+    //   turn.addClass('cat')
+    //   turn.attr("src", "cat.png")
+    // } else {
+    //   turn.addClass('dog')
+    //   turn.attr("src", "dog.jpg")
+    //   game.current_player = 2
+    // }
+  }
+
   // $(".player").click(function() {
   //   game.first_player(this)
+  //   image.addClass('cat')
+  //   image.attr('src', 'cat.png')
+  //
+  //   image.addClass('dog')
+  //   image.attr('src', 'dog.png')
   // })
+  // game.first_player(first_player)
+
+  TicTacToe.prototype.square_free = function (checkbox) {
+    var image = $(checkbox).find('.image')
+    if (image.hasClass('cat') || image.hasClass('dog')) {
+      return true
+    }
+  }
+
+  TicTacToe.prototype.switch_player = function () {
+    if (game.current_player === 1) {
+      game.current_player = 2
+    } else {
+      game.current_player = 1
+    }
+  }
 
   TicTacToe.prototype.play = function (checkbox) {
-    // if square hasClass cat or dog, do nothing
-    // else addClass cat or dog, depending on current player
-    // var id = this.player.id
-    // var img = this.player.img
-    // $( "#" ).addClass( "img" );
     var image = $(checkbox).find('.image')
-
-    if(image.hasClass('cat') || image.hasClass('dog')) {
-      alert('choose an open square')
-    } else {
+    if (game.square_free) {
       if (game.current_player === 1) {
         image.addClass('cat')
         image.attr('src', 'cat.png')
-        game.current_player = 2
       } else {
         image.addClass('dog')
         image.attr('src', 'dog.jpg')
-        game.current_player = 1
       }
+      game.win()
+      game.switch_player()
+    } else {
+      alert("Please choose a free square.")
     }
-    game.win()
-
   }
 
   $('.checkbox').click(function() {
@@ -75,34 +92,63 @@ $(document).ready(function () {
       '#8': ('image cat' === $('.checkbox').find( '#8' )[0].className),
       '#9': ('image cat' === $('.checkbox').find( '#9' )[0].className)
     }
-    // var images = $('.checkbox').find( '#1' )[0].className
-    // console.log(cat_checkboxes['#1'])
 
-      if(cat_checkboxes['#1'] && cat_checkboxes['#2'] && cat_checkboxes['#3'] ) {
-        console.log('kitties rule, puppies drool!')
-      }
+    if( cat_checkboxes['#1'] && cat_checkboxes['#2'] && cat_checkboxes['#3'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#1'] && cat_checkboxes['#4'] && cat_checkboxes['#7'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#1'] && cat_checkboxes['#5'] && cat_checkboxes['#9'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#4'] && cat_checkboxes['#5'] && cat_checkboxes['#6'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#7'] && cat_checkboxes['#8'] && cat_checkboxes['#9'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#2'] && cat_checkboxes['#5'] && cat_checkboxes['#8'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#3'] && cat_checkboxes['#6'] && cat_checkboxes['#9'] ) {
+      alert('kitties rule, puppies drool!')
+    } else if ( cat_checkboxes['#3'] && cat_checkboxes['#5'] && cat_checkboxes['#7'] ) {
+      alert('kitties rule, puppies drool!')
+    }
 
-    //   if(board.find('#1').hasClass(pattern) && board.find('#2').hasClass(pattern) && board.find('#3').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#1').hasClass(pattern) && board.find('#4').hasClass(pattern) && board.find('#7').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#1').hasClass(pattern) && board.find('#5').hasClass(pattern) && board.find('#9').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#4').hasClass(pattern) && board.find('#5').hasClass(pattern) && board.find('#6').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#7').hasClass(pattern) && board.find('#8').hasClass(pattern) && board.find('#9').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#2').hasClass(pattern) && board.find('#5').hasClass(pattern) && board.find('#8').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#3').hasClass(pattern) && board.find('#6').hasClass(pattern) && board.find('#9').hasClass(pattern)) {
-    //     won = 1;
-    //   } else if (board.find('#3').hasClass(pattern) && board.find('#5').hasClass(pattern) && board.find('#7').hasClass(pattern)) {
-    //     won = 1;
-    //   }
-    //   return won;
-    // }
+    var dog_checkboxes = {
+      '#1': ('image dog' === $('.checkbox').find( '#1' )[0].className),
+      '#2': ('image dog' === $('.checkbox').find( '#2' )[0].className),
+      '#3': ('image dog' === $('.checkbox').find( '#3' )[0].className),
+      '#4': ('image dog' === $('.checkbox').find( '#4' )[0].className),
+      '#5': ('image dog' === $('.checkbox').find( '#5' )[0].className),
+      '#6': ('image dog' === $('.checkbox').find( '#6' )[0].className),
+      '#7': ('image dog' === $('.checkbox').find( '#7' )[0].className),
+      '#8': ('image dog' === $('.checkbox').find( '#8' )[0].className),
+      '#9': ('image dog' === $('.checkbox').find( '#9' )[0].className)
+    }
+
+    if( dog_checkboxes['#1'] && dog_checkboxes['#2'] && dog_checkboxes['#3'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#1'] && dog_checkboxes['#4'] && dog_checkboxes['#7'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#1'] && dog_checkboxes['#5'] && dog_checkboxes['#9'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#4'] && dog_checkboxes['#5'] && dog_checkboxes['#6'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#7'] && dog_checkboxes['#8'] && dog_checkboxes['#9'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#2'] && dog_checkboxes['#5'] && dog_checkboxes['#8'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#3'] && dog_checkboxes['#6'] && dog_checkboxes['#9'] ) {
+      alert('WOOF WOOF WOOF!')
+    } else if ( dog_checkboxes['#3'] && dog_checkboxes['#5'] && dog_checkboxes['#7'] ) {
+      alert('WOOF WOOF WOOF!')
+    }
+
   }
 
+
+  function reset(table) {
+    table.find('td').each(function() {
+      $(this).removeClass('circle').removeClass('cross');
+    });
+  }
 
 
   TicTacToe.prototype.draw = {
